e058f2dc99ad60a86d1991d79b181ad1ba3e08f5
oscarraveloxt3-debug <oscarraveloxt3@gmail.com>
2025-11-20 17:12:12 -0500

    Commit 2: Implementación del menú interactivo

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido a la Calculadora");
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==== CALCULADORA ====");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        System.out.print("Seleccione una opción: ");
+
+        int opcion = sc.nextInt();
+    }
+
 
 
     }
