6e17760f81b1284e445758705845cb0bff9b9f75
oscarraveloxt3-debug <oscarraveloxt3@gmail.com>
2025-11-20 17:11:22 -0500

    Commit 1: Estructura base del proyecto y clase Main

---
+++ src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a la Calculadora");
+
+
+
+    }
+
+
+
+}
+
+
+
